c910edcc9fc7a3b281ccea8ea0c9b4866b5b8752
metivishvili1999 <sandro.metivishvili.1@btu.edu.ge>
2022-03-09 02:25:29 +0400

    added submitpage

--- src/components/Submit.js
+++ src/components/Submit.js
@@ -1,7 +1,14 @@
-function Submit () {
-    return (
-        <div className="submit">
+import React from 'react';
+import '../App.css';
 
+
+const Submit = ({prevPage,submitApplication}) => {
+    return (
+        <div className="submitPage">
+            <div>
+            <button className="submit-btn" onClick={submitApplication}>Submit</button>
+            <button className="submit-go-back-btn" onClick={prevPage}>go back</button>
+            </div>
         </div>
     )
 }
