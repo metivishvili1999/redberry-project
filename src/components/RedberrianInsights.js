e877d2e74d2e743f418ae522f01c98d7996adfaa
metivishvili1999 <sandro.metivishvili.1@btu.edu.ge>
2022-03-09 02:18:16 +0400

    redberrians final

--- src/components/RedberrianInsights.js
+++ src/components/RedberrianInsights.js
@@ -6,105 +6,119 @@ import ellipse1 from '../assets/Ellipse 1.svg';
 import ellipse2 from '../assets/Ellipse 2.svg';
 
 
-const RedberrianInsights = ({prevPage, nextPage}) => {
+const RedberrianInsights = ({prevPage, nextPage, handleRedberrianInsights, userObject}) => {
 
-    const [insightForm, setInsightForm] = useState ({
-        will_organize_devtalk: '',
-        devtalk_topic: '',
-        something_special: ''
-    })
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setInsightForm({ ...insightForm, [name]: value });
-      };
+  const [radioQuestion, setRadioQuestion] = useState('');
+  const [devtalkQuestion, setDevtalkQuestion] = useState('');
+  const [specialQuestion, setSpecialQuestion] = useState('');
 
-    const [insightErr, setInsightErr] = useState({});
+  const [radioQuestionsErr, setRadioQuestionsErr] = useState('');
+  const [devtalkQuestionErr, setDevtalkQuestionErr] = useState('');
+  const [specialQuestionErr, setSpecialQuestionErr] = useState('');
 
-    const insightValidation = () => {
-        setInsightErr('');
-        if(insightForm.will_organize_devtalk === ''){
-            setInsightErr(' this field is required');
-        }else if(insightForm.devtalk_topic === '') {
-            setInsightErr( 'this field is required')
-        }else if(insightForm.something_special === '') {
-            setInsightErr(' this field is required')
-        }else { 
-            
-            nextPage();
+
+  useEffect(() => {
+    if(userObject.devtalk_topic){
+      setDevtalkQuestion(userObject.devtalk_topic);
+    }
+    if(userObject.something_special){
+      setSpecialQuestion(userObject.something_special);
+    }
+  }, [userObject])
+
+
+  const handleDevtalkValue = (value) => {
+    if(value === 'yes'){
+      setRadioQuestion(true);
+    }else{
+      setRadioQuestion(false);
+    }
+  }
+
+
+  const radioValidation = () => {
+    if(radioQuestion !== true && radioQuestion !== false){
+      setRadioQuestionsErr('this field is required')
+      return false
+    } else {
+      return true;
+    }
+  }
+
+  const devtalkValidation = () => {
+    if(devtalkQuestion === '') {
+      setDevtalkQuestionErr('this field is required')
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  const specialValidation = () => {
+    if(specialQuestion === ''){
+      setSpecialQuestionErr('this field is required')
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+    const redberrianValidation = () => {
+      setRadioQuestionsErr('');
+      setDevtalkQuestionErr('');
+      setSpecialQuestionErr('');
+        if(radioValidation() && devtalkValidation() && specialValidation()) {
+          const devtalkObj = {
+            radioQuestion,
+            devtalkQuestion,
+            specialQuestion
+          }
+          handleRedberrianInsights(devtalkObj);
+          nextPage();
         }
     }
 
 
 
-
-
-
-
-
     return (
-        <div className='container'>
+        <div className='redberrian-container'>
           <div className='redberrian-left-panel'>
-            <div className='left-panel-text'>
+            <div className='redberrian-left-panel-text'>
                 What about you?
             </div>
-            <div className='questions-pos'>
-            <div className="radio-question">
-              <h3 className='radio-question-title' >Would you attend Devtalks and maybe also organize your own?</h3>
-              <div>
-                <div className='radio-input'>
-                  <input
-                    type="radio"
-                    name="will_organize_devtalk"
-                    id="willOrganize"
-                    value="true"
-                    onChange={handleChange}
-                  />
-                  <label htmlFor="willOrganize">Yes</label>
-                </div>
-                <div>
-                  <input
-                    type="radio"
-                    name="will_organize_devtalk"
-                    id="willNotOrganize"
-                    value="false"
-                    onChange={handleChange}
-                  />
-                  <label htmlFor="willNotOrganize">No</label>
-                </div>
-                <p className="errors">{insightErr.will_organize_devtalk}</p>
+            <div className='redberrian-form'>
+              <div className="radio-quest">
+                <p className="quest-title">Would you attend Devtalks and maybe also organize your own?</p>
+                {radioQuestionsErr && <p className="quest-validation" style={{ padding: 0 }}>{radioQuestionsErr}</p>}
+                <form onChange={({ target }) => handleDevtalkValue(target.value)}>
+                  <div>
+                    <input id="devtalk_yes" type="radio" name="devtalk" value="yes" />
+                    <label htmlFor="devtalk_yes">Yes</label>
+                  </div>
+                  <div>
+                    <input id="devtalk_no" type="radio" name="devtalk" value="no" />
+                    <label htmlFor="devtalk_no">No</label>  
+                  </div>
+                </form>
+              </div>
+
+              <div className="devtalk-quest">
+                <p className="quest-title">What would you speak about at Devtalk?</p>
+                {devtalkQuestionErr && <p className="quest-validation" style={{ padding: 0 }}>{devtalkQuestionErr}</p>}
+                <textarea onChange={({ target }) => setDevtalkQuestion(target.value)} 
+                  placeholder="I would..." value={devtalkQuestion}
+                  style={{ borderColor: devtalkQuestionErr ? '#FE3B1F' : '#525557' }} /> 
+              </div>
+
+              <div className='special-quest'>
+                <p className="quest-title">Tell us something special</p>
+                {specialQuestionErr && <p className="quest-validation" style={{ padding: 0 }}>{specialQuestionErr}</p>}
+                <textarea onChange={({ target }) => setSpecialQuestion(target.value)}
+                  placeholder="I..." className="bottom-textarea" value={specialQuestion}
+                  style={{ borderColor: specialQuestionErr ? '#FE3B1F' : '#525557' }} />
               </div>
             </div>
-            <div className="devtalk-question">
-              <label className='devtalk-title' htmlFor="devtalkTopic">
-                What would you speak about at Devtalk?
-              </label>
-              <textarea
-              className='devtalk-input'
-                name="devtalk_topic"
-                id="devtalkTopic"
-                placeholder="I would..."
-                cols="30"
-                rows="10"
-                onChange={handleChange}
-              ></textarea>
-            </div>
-            <p className="errors">{insightErr.devtalk_topic}</p>
-            <div className="special-question">
-              <label className='special-title' htmlFor="special">Tell us something special</label>
-              <textarea
-              className='special-input'
-                name="something_special"
-                id="special"
-                placeholder="I..."
-                cols="30"
-                rows="10"
-                onChange={handleChange}
-              ></textarea>
-            </div>
-            <p className="errors">{insightErr.something_special}</p>
-          </div>
-
 
             <div className="pagination">
                 <img src={prev} onClick={prevPage} className="prevnext" />
@@ -113,12 +127,12 @@ const RedberrianInsights = ({prevPage, nextPage}) => {
                 <img src={ellipse2} className="pagination-steps" />
                 <img src={ellipse1} className="pagination-steps" />
                 <img src={ellipse2} className="pagination-steps" />
-                <img src={next} onClick={insightValidation} className="prevnext" />
+                <img src={next} onClick={redberrianValidation} className="prevnext" />
             </div>
           </div>
-          <div className='skills-right-panel'>
-            <h1 className='skills-right-panel-title'>Redberrian Insights</h1>
-            <p className='skills-right-panel-text'>
+          <div className='redberrian-right-panel'>
+            <h1 className='redberrian-right-panel-title'>Redberrian Insights</h1>
+            <p className='redberrian-right-panel-text'>
             We were soo much fun before the pandemic started! Parties almost every weekend and lavish employee birthday
             celebrations! Unfortunately, covid ruined that fun like it did almost everything else in the world. But we try
             our best to zhuzh it up a bit. For example, we host biweekly Devtalks where our senior and lead developers talk
